8176f7264a15782d0637cc87842e3123e20473ba
kgupta01 <khushinu21@gmail.com>
2025-03-22 14:18:23 +0530

    Store the DailyWage With TotalWage in Array

--- Employee.js
+++ Employee.js
@@ -67,3 +67,20 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS
 }
 empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+
+//UC6
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+totalEmpHrs = 0;
+totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+empWage = calcDailyWage(totalEmpHrs);
+console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
